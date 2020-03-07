d0cda1775f26e23e0ed7072d2beb37e8ea4b1e9d
James2250 <Kaiserkid2010@aol.com>
2020-03-07 13:31:21 -0800

    Add files via upload

---
+++ ConvertMusic.py
@@ -0,0 +1,200 @@
+import shutil
+import time
+import os.path
+import sys
+
+from zipfile import ZipFile 
+
+SampleFolderName = ""
+TxtFileWorkingOn = ""
+ZipFileName = ""
+
+ListOfTxtFilePaths = []  #text files per zip folder
+ListOfTxtFileNames = []
+
+ListOfBrrFiles = []
+ListOfCopiedBrrFiles = []
+ListOfCopiedBrrFolders = [] 
+ListOfCopiedBrrNames = []
+
+#Taken from AddMusicK
+def SNESToPC(addr):			
+	if addr < 0 or addr > 0xFFFFFF or (addr & 0xFE0000) == 0x7E0000 or (addr & 0x408000) == 0x000000:		
+		return -1
+		
+	addr = ((addr & 0x7F0000) >> 1 | (addr & 0x7FFF))
+	return addr
+
+def ReadROMData(File):	
+	address = SNESToPC(0x0E8000) #addmusic @amk
+	address = address + 512  #200 hex header
+	
+	File.seek(address, 0)  #@amk 
+	amkTest = File.read(4)
+	
+	if amkTest[0] == 64:  # @ symbol
+		File.read(4) # skip forward to music pointer
+		
+	##---------------------------------##
+		data = File.read(1).hex()
+		data2 = File.read(1).hex()
+		data3 = File.read(1).hex()
+		
+		addr = data3 + data2 + data  #main music pointer
+		addr = "0x" + addr
+
+		IntAddr = int(addr, 0)
+		
+		IntAddr = SNESToPC(IntAddr) 
+		IntAddr = IntAddr + 512  #header
+		
+		File.seek(IntAddr, 0) 
+		File.read(30)   #specific song pointer we want is 30 bytes ahead, 00 00 00 00 before then
+		
+	##---------------------------------##
+		data4 = File.read(1).hex()
+		data5 = File.read(1).hex()
+		data6 = File.read(1).hex()
+		
+		NewAddr = data6 + data5 + data4  #main music pointer
+		NewAddr = "0x" + NewAddr
+		
+		IntAddr2 = int(NewAddr, 0)
+		
+		IntAddr2 = SNESToPC(IntAddr2) 
+		IntAddr2 = IntAddr2 + 512  #header
+		
+		IntAddr2 -= 4  #go back to size after STAR
+		File.seek(IntAddr2, 0) 
+		
+		size1 = int("0x" + File.read(1).hex(), 0)    #STAR size 1  
+		size2 = int("0x" + File.read(1).hex(), 0)    #STAR size 2 
+
+		File.read(2)  #ignore inverse size
+		
+		TotalSize = size1 | size2 << 8
+		FinalData = (File.read(TotalSize).hex())
+		
+		print(ZipFileName + " -- " + TxtFileWorkingOn + " : " + FinalData)
+		
+		File.close()
+		
+		os.remove("ROM.smc")	
+		os.remove("ROM.msc")
+		os.remove("ROM.smc~")
+		
+	else:
+		print(ZipFileName + " -- " + TxtFileWorkingOn + " FAILED TO PATCH " )
+
+def ExtractSongFromZip(path):
+	global SampleFolderName
+	global TxtFileWorkingOn
+	
+	brrFolderNames = set()
+	
+	for (dirpath, dirnames, filenames) in os.walk(path):
+		if "__MACOSX" in dirnames:
+			dirnames.remove("__MACOSX")
+			
+		if "SPCs" in dirnames:
+			dirnames.remove("SPCs")
+			
+		for filename in filenames:
+			if filename.endswith(".brr"):  # we are in samples folder
+				ListOfBrrFiles.append(dirpath + "/" + filename)
+				ListOfCopiedBrrNames.append(filename)
+				
+				#if not SeenBrrFile:
+				if os.path.basename(dirpath) not in brrFolderNames:
+					SeenBrrFile = True 
+					NewFolder = "./samples/" + os.path.basename(dirpath)
+					shutil.copytree(dirpath, NewFolder)
+					
+					ListOfCopiedBrrFolders.append(NewFolder)
+					brrFolderNames.add(os.path.basename(dirpath))
+					
+					SampleFolderName = NewFolder
+				
+			if filename.endswith('.txt'): 
+				if "readme" in filename.lower(): #ignore  
+					continue
+				
+				elif "patterns" in filename.lower(): #ignore 
+					continue
+					
+				else:  # .txt file song
+					ListOfTxtFilePaths.append(dirpath + "/" + filename)
+					ListOfTxtFileNames.append(filename)  
+					
+	count = 0
+	brr_count = 0
+	
+	for File in ListOfBrrFiles:	
+		shutil.copy(File, "./samples")
+		ListOfCopiedBrrFiles.append("./samples/" + ListOfCopiedBrrNames[brr_count])
+		
+		brr_count +=1 
+		
+	for File in ListOfTxtFilePaths:	
+		TxtFileWorkingOn = ListOfTxtFileNames[count]
+		shutil.copy(File,  "./music")
+		
+		OldName = "./music/" + ListOfTxtFileNames[count]
+		NewName = "./music/Song1.txt"
+		
+		if os.path.isfile(NewName):
+			os.remove(NewName)
+			
+		os.rename(OldName, NewName)
+		
+		shutil.copy("ROMo.smc",  "ROM.smc")
+		
+		#-noblock needed so cmd doesn't wait for pressing enter
+		os.system('cmd /c "AddmusicK ROM.smc" -noblock')
+		
+		File = open("ROM.smc", "rb")
+		ReadROMData(File)
+		
+		count += 1 
+
+def main():
+	sys.stdout = open('OutputFile.txt', 'w')
+
+	global ZipFileName
+	
+	for filename in os.listdir("./SONGS"):
+		os.mkdir("./SONGS/TEMP")
+		
+		ListOfTxtFileNames.clear()  
+		ListOfTxtFilePaths.clear()  
+
+		ListOfBrrFiles.clear()
+		ListOfCopiedBrrNames.clear()
+		ListOfCopiedBrrFiles.clear()
+		ListOfCopiedBrrFolders.clear()
+		
+		if filename.endswith(".zip"): 
+			ZipFileName = filename
+			
+			with ZipFile("./SONGS/" + filename, 'r') as zipObj:
+				try:
+					zipObj.extractall('./SONGS/TEMP')
+					ExtractSongFromZip("./SONGS/TEMP")
+					
+				except OSError as e:
+					print(filename + " FAILED TO OPEN")  
+			
+		for File in ListOfCopiedBrrFiles:	
+			if os.path.isfile(File):
+				os.remove(File)
+			
+		for Folder in ListOfCopiedBrrFolders:	
+			if os.path.isdir(Folder):
+				shutil.rmtree(Folder) 
+				
+		shutil.rmtree("./SONGS/TEMP")  #can't use os.rmdir, needs empty folder
+		
+
+if __name__== "__main__":
+	main()
+	
---
+++ Readme.txt
@@ -0,0 +1,37 @@
+Using Python 3 
+
+--- IdentifySongs.py --- 
+
+Just need to run and give it "ROMName".smc.  Will create an output file with all of the known songs for levels/submaps as 
+long as the ROM used AddMusicK.  
+
+- Needs the SMW_Songs.txt database file in the same directory.  
+
+
+
+
+--- ConvertMusic.py --- 
+
+Doesn't need to be used unless you are trying to add more custom songs into the database.  
+
+Helped create the SMW_Songs database. Will take a folder of custom songs in zip format from 
+https://www.smwcentral.net/?p=section&s=smwmusic and output 1 file of all of the songs in SMW format (OutputFile.txt).   
+
+- Needs a folder called SONGS with all of the zips in it to be converted.   
+
+- Needs to be in the same folder as AddmusicK.exe.  
+
+- Add Song1.txt to the end of Addmusic_list.txt in the AddmusicK folder.  
+  Is what ConvertMusic renames each of the song text files to when going through the zip folders.
+  
+- Needs to have an expanded ROM called ROMo.smc in the same directory (save a level in lunar magic or use lunar expand)  
+  Not provided here.  
+
+- Doesn't handle songs with sample groups, but handles all other samples.  Copies any samples found to the AddMusicK samples
+  folder and then deletes them after use.  
+  
+- Does not support any of the __MACOSX songs.  
+
+
+AddMusicK
+https://www.smwcentral.net/?p=section&a=details&id=17546
